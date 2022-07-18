ed7f4ff58ada8b727cb4f6809b4e5293b3f7b533
zainazeem <61400299+zainazeem@users.noreply.github.com>
2022-07-18 13:27:20 -0400

    Cleanup

--- components/Auth.js
+++ components/Auth.js
@@ -14,9 +14,7 @@ export default function Auth() {
       if (evt.data.keyri && evt.data.data && document.location.origin == evt.origin) {
         const { data } = evt;
         if (!data.error) {
-          console.log(JSON.parse(data.data));
           let refresh_token = JSON.parse(data.data).refreshToken;
-          console.log(refresh_token);
           await handleQrLogin(refresh_token);
         } else if (data.error) {
           console.log(`Keyri error: ${data.message}`);
